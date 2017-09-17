b37f2e22a1a4311623e0a80090b26bb4eb95da5a
Jingjie Yang <j.yang19@ejm.org>
2017-09-17 22:54:49 +0200

    hosting on firebase now

--- src/publish/pushToMaster.js
+++
@@ -1,8 +0,0 @@
-const ghpages = require('gh-pages');
-
-ghpages.publish('build', {
-        branch: 'master'
-    }, (err) => {
-        console.log(err ? err : "Pushed build/ successfully to master branch.");
-    }
-);
